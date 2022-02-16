7d633e4e78848cc2a0112a5fc5499c9228c92d5c
nilammone <nilammone.bannavong@gmail.com>
2022-02-16 20:29:30 +0700

    update users

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,9 +11,11 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $user = User::create([
-            'name' => $request->name,
+            'username' => $request->username,
             'email' => $request->email,
-            'password' => bcrypt($request->password)
+            'password' => bcrypt($request->password),
+            'user_emp_id' => $request->user_emp_id,
+            'isAdmin' => '2'
         ]);
 
         return $user;
